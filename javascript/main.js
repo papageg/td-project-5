1fe9d53b43541b6ecd44b8a489f55a03e59c2224
Greg <greggordon98@gmail.com>
2019-01-09 12:42:00 -0800

    hmm
    hmm

--- javascript/main.js
+++ javascript/main.js
@@ -41,25 +41,25 @@ $('.gallery').append(cardHTML);
 var $card = $('#gallery .card');
 
 for (var i = 0 ; i <= $card.length; i++) {
-$card[i].addEventListener('click', clickCard , false) ;
-   function clickCard() {
-     empClickHTML = `<div class="modal-container">`;
-     empClickHTML += `<div class="modal">`;
-     empClickHTML += `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
-     empClickHTML += `<div class="modal-info-container">`;
-     empClickHTML += '<img class="modal-img" src="' + result.picture.medium  + '"alt="profile picture">';
-     empClickHTML += '<h3 id="' + name + '" class="modal-name cap">' + result.name.first + ' '+ result.name.last + '</h3>';
-     empClickHTML += '<p class="modal-text">' + result.email + '</p>';
-     empClickHTML += '<p class="modal-text cap">' + result.location.city+ '</p>';
-     empClickHTML += `<hr>`;
-     empClickHTML +=  '<p class="modal-text">' + result.phone +'</p>';
-     empClickHTML += '<p class="modal-text">' + result.location.street + '' + result.location.city + ' ' + result.location.state + ' ' + result.location.postcode +  '</p>';
-     empClickHTML += '<p class="modal-text">' + result.dob + '</p>';
-     empClickHTML += `</div>`;
-     empClickHTML += `</div>`;
+$($card[i]).click( function clickCard() {
+   empClickHTML = `<div class="modal-container">`;
+   empClickHTML += `<div class="modal">`;
+   empClickHTML += `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
+   empClickHTML += `<div class="modal-info-container">`;
+   empClickHTML += '<img class="modal-img" src="' + result.picture.medium  + '"alt="profile picture">';
+   empClickHTML += '<h3 id="' + name + '" class="modal-name cap">' + result.name.first + ' '+ result.name.last + '</h3>';
+   empClickHTML += '<p class="modal-text">' + result.email + '</p>';
+   empClickHTML += '<p class="modal-text cap">' + result.location.city+ '</p>';
+   empClickHTML += `<hr>`;
+   empClickHTML +=  '<p class="modal-text">' + result.phone +'</p>';
+   empClickHTML += '<p class="modal-text">' + result.location.street + '' + result.location.city + ' ' + result.location.state + ' ' + result.location.postcode +  '</p>';
+   empClickHTML += '<p class="modal-text">' + result.dob + '</p>';
+   empClickHTML += `</div>`;
+   empClickHTML += `</div>`;
 
-     $('body').append(empClickHTML);
-   }
+   $('body').append(empClickHTML);
+ }) ;
+
 
    }
 

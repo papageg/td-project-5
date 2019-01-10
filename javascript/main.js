1930dee2a525e3bb6eb0ac476a01c801670b7cab
Greg <greggordon98@gmail.com>
2019-01-10 12:18:56 -0800

    lost
    lost

--- javascript/main.js
+++ javascript/main.js
@@ -5,12 +5,11 @@
 //     console.log(data);
 //   }
 // });
+var arrayCards = [];
 
-
-$.getJSON('https://randomuser.me/api/?results=12', function(data){
+$.getJSON('https://randomuser.me/api/?results=12', function (data){
   let cardHTML;
   let searchHTML;
-  let empClickHTML;
 
 
 
@@ -20,6 +19,7 @@ $.getJSON('https://randomuser.me/api/?results=12', function(data){
   searchHTML += '<input type="submit" value="&#x1F50D;" id="serach-submit" class="search-submit"></form>'
   $('.search-container').append(searchHTML);
 //END SEARCH
+
 
 
 
@@ -34,88 +34,47 @@ $.getJSON('https://randomuser.me/api/?results=12', function(data){
     cardHTML += '<p class ="card-text cap">' + result.location.city+ ", " + result.location.state + '</p></div>';
     cardHTML += '</div>'
 
-
-
 $('.gallery').append(cardHTML);
-
-var $card = $('.gallery');
-
-for (var i = 0 ; i <= $card.length; i++) {
-$($('.gallery')[i]).click( function clickCard() {
-   empClickHTML = `<div class="modal-container">`;
-   empClickHTML += `<div class="modal">`;
-   empClickHTML += `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
-   empClickHTML += `<div class="modal-info-container">`;
-   empClickHTML += '<img class="modal-img" src="' + result.picture.medium  + '"alt="profile picture">';
-   empClickHTML += '<h3 id="' + name + '" class="modal-name cap">' + result.name.first + ' '+ result.name.last + '</h3>';
-   empClickHTML += '<p class="modal-text">' + result.email + '</p>';
-   empClickHTML += '<p class="modal-text cap">' + result.location.city+ '</p>';
-   empClickHTML += `<hr>`;
-   empClickHTML +=  '<p class="modal-text">' + result.phone +'</p>';
-   empClickHTML += '<p class="modal-text">' + result.location.street + '' + result.location.city + ' ' + result.location.state + ' ' + result.location.postcode +  '</p>';
-   empClickHTML += '<p class="modal-text">' + result.dob + '</p>';
-   empClickHTML += `</div>`;
-   empClickHTML += `</div>`;
-
-   $('body').append(empClickHTML);
- }) ;
+arrayCards.push(cardHTML);
 
 
-   }
-
-//
-// empClickHTML = `<div class="modal-container">`;
-// empClickHTML += `<div class="modal">`;
-// empClickHTML += `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
-// empClickHTML += `<div class="modal-info-container">`;
-// empClickHTML += '<img class="modal-img" src="' + result.picture.medium  + '"alt="profile picture">';
-// empClickHTML += '<h3 id="' + name + '" class="modal-name cap">' + result.name.first + ' '+ result.name.last + '</h3>';
-// empClickHTML += '<p class="modal-text">' + result.email + '</p>';
-// empClickHTML += '<p class="modal-text cap">' + result.location.city+ '</p>';
-// empClickHTML += `<hr>`;
-// empClickHTML +=  '<p class="modal-text">' + result.phone +'</p>';
-// empClickHTML += '<p class="modal-text">' + result.location.street + '' + result.location.city + ' ' + result.location.state + ' ' + result.location.postcode +  '</p>';
-// empClickHTML += '<p class="modal-text">' + result.dob + '</p>';
-// empClickHTML += `</div>`;
-// empClickHTML += `</div>`;
-// $('body').append(empClickHTML);
-
-});
-  //  $('.card').addEventListener('click', function() {
-
-//   var carcard = $('#gallery .card');
-//   console.log(carcard[1])
-// });
-
-
-
-
-
-
-
+})
+//let clickedCard;
 
 
 });
 
- // $('.card').addEventListener('click', function() {
- //   empClickHTML = `<div class="modal-container">`;
- //   empClickHTML += `<div class="modal">`;
- //   empClickHTML += `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
- //   empClickHTML += `<div class="modal-info-container">`;
- //   empClickHTML += '<img class="modal-img" src="' + result.picture.medium  + '"alt="profile picture">';
- //   empClickHTML += '<h3 id="' + name + '" class="modal-name cap">' + result.name.first + ' '+ result.name.last + '</h3>';
- //   empClickHTML += '<p class="modal-text">' + result.email + '</p>';
- //   empClickHTML += '<p class="modal-text cap">' + result.location.city+ '</p>';
- //   empClickHTML += `<hr>`;
- //   empClickHTML +=  '<p class="modal-text">' + result.phone +'</p>';
- //   empClickHTML += '<p class="modal-text">' + result.location.street + '' + result.location.city + ' ' + result.location.state + ' ' + result.location.postcode +  '</p>';
- //   empClickHTML += '<p class="modal-text">' + result.dob + '</p>';
- //   empClickHTML += `</div>`;
- //   empClickHTML += `</div>`;
- //   $('body').append(empClickHTML);
- // });
+let empClickHTML;
+function empButton(empClickHTML) {
+    empClickHTML = `<div class="modal-container">`;
+    empClickHTML += `<div class="modal">`;
+    empClickHTML += `<button type="button" id="modal-close-btn" class="modal-close-btn"><strong>X</strong></button>`;
+    empClickHTML += `<div class="modal-info-container">`;
+    empClickHTML += '<img class="modal-img" src="' +  result.picture.medium + '"alt="profile picture">';
+    empClickHTML += '<h3 id="' + name + '" class="modal-name cap">' + result.name.first + ' '+ result.name.last + '</h3>';
+    empClickHTML += '<p class="modal-text">' + result.email + '</p>';
+    empClickHTML += '<p class="modal-text cap">' + result.location.city+ '</p>';
+    empClickHTML += `<hr>`;
+    empClickHTML +=  '<p class="modal-text">' + result.phone +'</p>';
+    empClickHTML += '<p class="modal-text">' + result.location.street + '' + result.location.city + ' ' + result.location.state + ' ' + result.location.postcode +  '</p>';
+    empClickHTML += '<p class="modal-text">' + result.dob + '</p>';
+    empClickHTML += `</div>`;
+    empClickHTML += `</div>`;
+    $('body').append(empClickHTML);
+};
 
 
+
+if($('div .card').click() === true) {
+  console.log('worked');
+}
+for (i = 0; i <= 12; i++){
+  var dude = i - $(".card").val();
+  var selectCard = document.querySelectorAll('.card');
+  $(selectCard[dude]).append(empClickHTML);
+
+console.log($(selectCard[dude]));
+}
 //END CARD
 
 //START EMPLOYEE CLICKED THIS IS WHERE THE POPUP IS WHEN YOU CLICK AN EMPLOYEE
